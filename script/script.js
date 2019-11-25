a039b876172e800f20df6c75d214a7d1634a0cfc
Sarah Armitage <saraharmitage314@gmail.com>
2019-11-25 16:53:52 -0500

    finalized input combinations

--- script/script.js
+++ script/script.js
@@ -38,41 +38,70 @@ movieApp.getGenreId = function (movieGenre) {
 // URL for movie posters
 let apiImagePath = "https://image.tmdb.org/t/p/w500";
 
-    // `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=${movieApp.selectedGenreID}`;
+// Function used to GET MOVIES BASED ON GENRE AND YEAR (Discover)
+movieApp.getMoviesDiscover = function () {
+// API PARAMS *WASN'T WORKING IN GLOBAL SCOPE* 
+    let genreTime;
+    let personId;
+    // if the genre ID is equal to an empty string, set genreTime to be an empty string, otherwise set genreTime equal to the ID
+    // converts it from undefined to ""
+    if (movieApp.selectedGenreID === "") {
+        genreTime = "";
+    } else {
+        genreTime = movieApp.selectedGenreID;
+    }
 
-// API CALL TO GET MOVIES BASED ON GENRE =========================
-// movieApp.getMovies = function () {
+    const apiKey = "20e7d413ebcc68553bf10a0da5428763";
+    const apiUrl = `https://api.themoviedb.org/3/discover/movie`;
 
-// // API PARAMS *WASN'T WORKING IN GLOBAL SCOPE* ===========
-//     const apiKey = "20e7d413ebcc68553bf10a0da5428763";
-//     // const apiUrl = `https://api.themoviedb.org/3/discover/movie/?api_key=${apiKey}&with_genres=${movieApp.selectedGenreID}&include_adult=falseith_original_languwage='en-US'`;
-//      const apiUrl = `https://api.themoviedb.org/3/discover/movie`;
-    
+    return $.ajax({
+        url: apiUrl,
+        method: 'GET',
+        dataType: 'json',
+        data: {
+            api_key: apiKey,
+            format: 'json',
+            // with_genres: `${movieApp.selectedGenreID}`,
+            with_genres: genreTime,
+            include_adult: false,
+            with_original_language: 'en',
+            primary_release_year: movieApp.year,
+            with_cast: movieApp.actorId
+        }
+    }).then(function(resultsObject) {
+        const movieResults = resultsObject; 
+        const resultLength = movieResults.results.length
+        const randomNumber = (Math.floor(Math.random() * resultLength));
+        const filteredResults = movieResults.results[randomNumber];
+        console.log(filteredResults);
+        // movieApp.displayMovies(filteredResults);
 
-//     return $.ajax({
-//         url: apiUrl,
-//         method: 'GET',
-//         dataType: 'json',
-//         data: {
-//             api_key: apiKey,
-//             format: 'json',
-//             with_genres: `${movieApp.selectedGenreID}`,
-//             include_adult: false,
-//             with_original_language: 'en',
-//             primary_release_year: movieApp.year
-//         }
-//     }).then(function(resultsObject) {
-//         const movieResults = resultsObject; 
-//         const resultLength = movieResults.results.length
-//         const randomNumber = (Math.floor(Math.random() * resultLength));
-//         const filteredResults = movieResults.results[randomNumber];
-//         console.log(filteredResults);
-//         movieApp.displayMovies(filteredResults);
-//     });
-// }; 
+        // if there are no results
+        if (resultsObject.total_results === 0) {
+            console.log("No results for you!");
+            $('.resultsContent').append(
+                `
+                <div class="resultsInfo">
+                    <div class="infoContainer">
+                        <h2>No results for you!</h2>
+                        <p>We couldn't find any movies that match your selection, please choose again!</p>
+                    </div>
+                    <div class="imageContainer">
+                        <img src="./assets/noMovie.jpg">
+                    </div>
+                </div>`
+            )
+        } else {
+            movieApp.displayMovies(filteredResults);
+        }
+    }).fail(function(error) {
+        console.log(error);
+        
+    })
+}; 
 
-// API CALL TO GET MOVIES BASED ON NAME =========================
-movieApp.getMovies = function () {
+// Function used to GET MOVIES BASED ON ACTOR/ACTRESS NAME (Search) 
+movieApp.getMoviesSearch = function () {
 
     // API PARAMS *WASN'T WORKING IN GLOBAL SCOPE* ===========
     const apiKey = "20e7d413ebcc68553bf10a0da5428763";
@@ -88,19 +117,27 @@ movieApp.getMovies = function () {
         data: {
             api_key: apiKey,
             format: 'json',
-            // with_genres: `${movieApp.selectedGenreID}`,
-            query: "Tom Cruise",
+            // query: "Tom Cruise",
+            query: movieApp.name,
             include_adult: false,
             with_original_language: 'en',
-            primary_release_year: movieApp.year
+            // primary_release_year: movieApp.year
         }
     }).then(function (resultsObject) {
-        const movieResults = resultsObject;
-        const resultLength = movieResults.results.length
-        const randomNumber = (Math.floor(Math.random() * resultLength));
-        const filteredResults = movieResults.results[randomNumber];
-        // console.log(filteredResults);
-        movieApp.displayMovies(filteredResults);
+        movieApp.actorId = resultsObject.results[0].id;
+        console.log(movieApp.actorId);
+        // console.log(resultsObject);
+
+        // make another API call based on ID to get all that persons movies
+        movieApp.getMoviesDiscover();
+
+
+        // const movieResults = resultsObject;
+        // const resultLength = movieResults.results.length
+        // const randomNumber = (Math.floor(Math.random() * resultLength));
+        // const filteredResults = movieResults.results[randomNumber];
+        // // console.log(filteredResults);
+        // movieApp.displayMovies(filteredResults);
     });
 }; 
 
@@ -126,20 +163,53 @@ movieApp.displayMovies = function(results) {
         // console.log(results.poster_path);
 }
 
-// RESET PAGE
+// Function to RESET PAGE
 movieApp.resetPage = function() {
-    let $reset = $('#selectedGenre');
+    let $resetGenre = $('#selectedGenre');
+    let $resetYear = $('#selectedYear');
+    let $resetName = $('#selectedName');
     // console.log($reset);
     
-    // reset genres drowp down
-    $reset[0].selectedIndex = 0;
+    // reset genres drop down to say "Genre:"
+    $resetGenre[0].selectedIndex = 0;
+
+    // reset year input 
+    $resetYear.val('');
+
+    // reset name input
+    $resetName.val('');
 
     // // reset results section
     // // remove current movie+info
     // $('.resultsInfo').remove();
+    $('.resultsInfo').remove();
 
     // call fxn to get new movie
     
+    // re-initialize variables
+    movieApp.actorId = "";
+
+    // after page is reset, call fxn that will determine which API call should be made (depends on which inputs are used by user)
+    movieApp.determineApiCall();
+}
+
+// Function to determine which API call should be made
+movieApp.determineApiCall = function () {
+    // if the user slected from ALL THREE inputs, then make two API calls (Discover & Search)
+    if (
+        movieApp.genre != "movie genre:" && movieApp.year != "" && movieApp.name != "" ||
+        movieApp.year != "" && movieApp.name != "" ||
+        movieApp.genre != "movie genre:" && movieApp.name != "" ||
+        movieApp.name != "") {
+        console.log("I picked a movie and a year and a person!");
+        movieApp.getMoviesSearch();
+
+        // else if the user selected from ONLY the genre and(or) year inputs, then make one API call (Discover) 
+        // genre, year, genre/year
+    } else if (movieApp.genre != "movie genre:" || movieApp.year != "" || movieApp.genre != "movie genre:" && movieApp.year != "") {
+        console.log("I picked a genre and a year!");
+        movieApp.getMoviesDiscover();
+    }
 }
 
 
@@ -149,18 +219,30 @@ movieApp.eventListeners = function () {
     // grab user input (selected drop-down menu option)
     $('form').on('submit', function (event) {
         event.preventDefault();
-        // 
+        console.log(`clicked button`);
+        
+        
+        // save user input to variabes
+        // GENRE (use the genre to get associated ID#)
         movieApp.genre = $('#selectedGenre option:selected').text().toLowerCase();
+        console.log(movieApp.genre);
+        movieApp.getGenreId(movieApp.genre);
+         // YEAR
         movieApp.year = $('#selectedYear').val();
         console.log(movieApp.year);
+        // ACTOR/ACTRESS NAME
+        movieApp.name = $('#selectedName').val();
+        console.log(movieApp.name);
         
         // get back a genre as a string
         // reset results section
         // remove current movie+info
-        $('.resultsInfo').remove();
+        // $('.resultsInfo').remove();
+        // movieApp.resetPage();
+        // movieApp.getGenreId(movieApp.genre);
+        // movieApp.getMovies();
+
         movieApp.resetPage();
-        movieApp.getGenreId(movieApp.genre);
-        movieApp.getMovies();
     });
 
     // event listener for reset button
@@ -173,8 +255,12 @@ movieApp.eventListeners = function () {
 // INITALIZE APP
 movieApp.init = function () {
     movieApp.eventListeners();
+    console.log("initialized");
+    
 }
 // DOC READY
 $(function () {
     movieApp.init();
+    console.log("doc ready");
+    
 });
